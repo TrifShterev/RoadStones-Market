fa820301f23cb095eb083edd054d0d7341679027
TrifShterev <tshterev@yahoo.com>
2021-06-30 20:01:31 +0300

    Added Delete and Edit functions and Fontawesome

--- RoadStones Market/Controllers/CategoryController.cs
+++ RoadStones Market/Controllers/CategoryController.cs
@@ -35,7 +35,80 @@ namespace RoadStones_Market.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category model)
         {
+            //Server Validation- Check
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
             _db.Categories.Add(model);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //GET
+        public IActionResult Edit(int id)
+        {
+            if (id==0 || id == null)
+            {
+                return NotFound();
+            }
+            var model = _db.Categories.Find(id);
+
+            if (model== null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category model)
+        {
+            //Server Validation- Check
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _db.Categories.Update(model);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var model = _db.Categories.Find(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var model = _db.Categories.Find(id);
+
+            //Server Validation- Check
+            if (model== null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(model);
             _db.SaveChanges();
 
             return RedirectToAction("Index");
